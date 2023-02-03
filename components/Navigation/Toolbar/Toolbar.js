2c2e444e048ec0a2c8dd86bf6775743819067edb
Luke Graham <luke.evan.graham@gmail.com>
2023-02-03 15:02:01 -0800

    data coming in

---
+++ components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,11 @@
+import LoginButton from "../LoginButton/LoginButton";
+
+const Toolbar = () => {
+  return (
+    <>
+      <LoginButton />
+    </>
+  );
+};
+
+export default Toolbar;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,51 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '@/styles/Home.module.css'
+import { useEffect, useState } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "./api/auth/[...nextauth]";
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import Toolbar from "@/components/Navigation/Toolbar/Toolbar";
+import { getSession } from "next-auth/react";
+import { getToken } from "next-auth/jwt";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const [userData, setUserData] = useState(null);
+  const [leaderData, setLeaderData] = useState(null);
+
+  useEffect(() => {
+    async function getUserData() {
+      let response = await getSession();
+      response = await response;
+      setUserData(response);
+      return userData;
+    }
+
+    async function prepLeader() {
+      let response = await getSession();
+      response = await response;
+      if (response) {
+        const jwt = response.accessToken;
+        console.log("jwt: ", jwt);
+
+        let leaderResponse = await fetch(
+          "https://admin.rbcommunity.org/leader-groups",
+          {
+            headers: { Authorization: `Bearer ${jwt}` },
+          }
+        );
+
+        leaderResponse = await leaderResponse.json();
+        setLeaderData(leaderResponse);
+      }
+    }
+
+    getUserData();
+    prepLeader();
+  }, []);
+
   return (
     <>
       <Head>
@@ -14,110 +54,30 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.js</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
-        </div>
+      <Toolbar />
+      <main>
+        <h1>Welcome to RB Community Church's Leader Site</h1>
+        <div>
+          {leaderData ? <h2>Elders</h2> : null}
 
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
+          {console.log("ED: ", leaderData)}
+          {leaderData
+            ? leaderData[0].users.map((elder) => (
+                <div style={{ marginBottom: "2rem" }} key={elder.id}>
+                  <div>{`${elder.firstName} ${elder.lastName}`}</div>
+                  <div>{elder.address}</div>
+                  <div>{`${elder.city}, CA ${elder.zipCode}`}</div>
+                  <div style={{ marginTop: ".33rem" }}>
+                    Cell Phone: {elder.cellPhone}
+                  </div>
+                  <div>Home Phone: {elder.homePhone}</div>
+                  <a href={`mailto:${elder.email}`}>{elder.email}</a>
+                  <div>Area of responsibility: {elder.responsibility}</div>
+                </div>
+              ))
+            : null}
         </div>
       </main>
     </>
-  )
+  );
 }
